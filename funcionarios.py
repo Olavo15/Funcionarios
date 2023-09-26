7968444ffc1d24deffb8307bef2e5a83a79f95d1
Olavo Regis <114032521+Olavo15@users.noreply.github.com>
2023-09-26 13:06:27 -0300

    Update funcionarios.py

--- funcionarios.py
+++ funcionarios.py
@@ -5,16 +5,15 @@ class Funcionario:
         self .horas = {}
         self .salario_hora = {}
     def cadastro_hora(self, mes, horas):
-        if(mes not in self.horas):
+        if(mes not in self .horas):
             self .horas[mes] = horas
     def cadastro_salario_hora(self, mes, valor):
-        if(mes not in self. salario_hora):
+        if(mes not in self .salario_hora):
             self .salario_hora[mes] = valor
     def calcula_salario(self, mes):
-        if(mes not in self .horas) or(mes not in self.salario_hora):
+        if(mes not in self .horas) or(mes not in self .salario_hora):
             print('Mês Inexistente!!')
         else:
-            return
-        self .horas[mes] * self .salario_hora[mes]
+            return self .horas[mes] * self .salario_hora[mes]
     def __repr__(self):
         return f'Funcionário:{self .nome},\nEmail:{self .email},\nhoras/mês:{self .horas},\nsalário-hora:{self .salario_hora}'
